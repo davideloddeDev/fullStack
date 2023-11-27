6ecddd3cfe8a765b0edf29c9a4756b204aba075f
Davide Lodde <johndoe@example.com>
2023-11-27 09:59:44 +0100

    aggiornamento file 27 novembre

---
+++ js-advanced/asincrono/promise1.js
@@ -0,0 +1,13 @@
+/*
+Riscrivi la funzione fetchDataFromAPI dell'esercizio Le Callback - 6 utilizzando Promises per una migliore gestione degli errori.
+La promise dovrebbe risolversi con i dati e rifiutare con un messaggio di errore.
+function fetchDataFromAPI() {
+  //....
+}
+
+fetchDataFromAPI()
+*/
+
+function fetchDataFromAPI() {
+    
+}
