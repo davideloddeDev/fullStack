a3cc89f7030a53dd6fc21825392f85ccf11f80d2
Davide Lodde <johndoe@example.com>
2023-12-04 15:56:29 +0100

    async_await

---
+++ js-advanced/asincrono/async_await1.js
@@ -0,0 +1,36 @@
+/*
+La funzione caricamentoDati simula il caricamento di dati asincroni con un ritardo di 2 secondi!
+
+Implementa la funzioneottieniDatiutilizzando await per attendere il caricamento dei dati, quindi visualizza i dati ottenuti in console.
+function caricamentoDati() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Questi sono i dati ottenuti dalla fonte asincrona.");
+    }, 2000);
+  });
+}
+
+// implementa la funzione asincrona "ottieniDati"
+*/
+
+function caricamentoDati() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Questi sono i dati ottenuti dalla fonte asincrona.");
+        }, 2000);
+    });
+}
+
+// implementa la funzione asincrona "ottieniDati"
+
+async function ottieniDati() {
+    try {
+        const data = await caricamentoDati()
+
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+ottieniDati()
