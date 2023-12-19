03ec1b5c361c56ebce8952e55d692446d6cac3d7
Davide Lodde <johndoe@example.com>
2023-12-19 23:07:03 +0100

    19 dicembre

---
+++ studioPersonale/product.js
@@ -0,0 +1,29 @@
+/*
+    array di oggetti di prodotti, prezzo, nome
+    simulare chiamata al database
+    funzione di acquisto prodotto
+    gestire gli errori
+    controllare la quantità è se disponibile scalare dall'inventario
+*/
+
+const product = [
+    {
+        id: 1,
+        price: 100,
+        name: "Smartphone"
+    },
+    {
+        id: 2,
+        price: 150,
+        name: "Tablet"
+    },
+    {
+        id: 3,
+        price: 200,
+        name: "PC"
+    }
+]
+
+function name(params) {
+    
+}
