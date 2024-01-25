aa3299ae15585e175dd72dd7a5da78c494fa88b9
Davide Lodde <johndoe@example.com>
2024-01-25 16:22:43 +0100

    init

--- js-basic/4. funzioni/esercizio29.js
+++
@@ -1,9 +0,0 @@
-// Scrivi una funzione di nome printName che semplicemente stampi il tuo nome.
-
-function printName() {
-    console.log("Davide Lodde");
-  }
-  
-  // Chiamata alla funzione per stampare il nome
-  printName();
-  
--- js-basic/5. gli oggetti/esercizio38.js
+++
@@ -1,9 +0,0 @@
-// Partendo dall'oggetto precedente, scrivi un ciclo for in dove stampiamo le chiavi dell'oggetto car
-let car = {
-    "name": "Davide",
-    "color": "Blue"
-};
-for (let key in car) {
-    console.log(key)
-    
-}
