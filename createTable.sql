735e6891f70d19e31c43fc161bbc72d07dc48ca8
Davide Lodde <johndoe@example.com>
2024-03-27 10:49:29 +0100

    create table

---
+++ createTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    genre VARCHAR(100),
+    published_year INT,
+    isbn VARCHAR(17),
+    price DECIMAL(10, 2),
+    rating DECIMAL(3, 2),
+    stock_count INT
+);
